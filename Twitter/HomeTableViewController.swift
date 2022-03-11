9b168e40d6f897b16949ee0fb1009f07fb515b21
nhi-nguyen-csis <nhi.nguyen.csis@gmail.com>
2022-03-11 14:12:20 -0800

    added functionalities to request user profile pic, username, and tweet text from API to show up on home screen

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -9,10 +9,44 @@
 import UIKit
 
 class HomeTableViewController: UITableViewController {
-
+    
+    //create an empty array of dictionary
+    var tweetArray =  [NSDictionary]()
+    var numberOfTweet: Int!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //after the screen is done loading, call the loadTweet() to request API
+        loadTweet()
     }
+    
+    
+    // func to call API
+    func loadTweet(){
+        let url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        // myParams to pass to the API to get exactly how many tweets you want
+        // count is the API's parameter
+        let myParams = ["count": 10]
+        // call the API to get multiple tweets --> need to use getDictionariesRequest, instead of getDictionaryRequest (1 tweet only)
+        TwitterAPICaller.client?.getDictionariesRequest(url: url, parameters: myParams, success:
+        { (tweets: [NSDictionary]) in
+            
+            self.tweetArray.removeAll()
+            for tweet in tweets {
+                self.tweetArray.append(tweet)
+            }
+            
+            // everytime we call API, we repopulate our list to reload the data with the new content
+            self.tableView.reloadData()
+            
+        }, failure: { Error in
+            print("Could not retrieve tweets! oh no!")
+        })
+        
+    }
+    
+    
     
     // this func is executed when users click on logout button
     @IBAction func onLogout(_ sender: UIBarButtonItem) {
@@ -27,15 +61,38 @@ class HomeTableViewController: UITableViewController {
         UserDefaults.standard.set(false, forKey: "userLoggedIn")
     }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // withIdentifier: name of the prototype cell
+        // cast it as TweetCellTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
+        
+        //extract user name from API call
+        //casting it to dictionary
+        let user = tweetArray[indexPath.row]["user"] as! NSDictionary
+        cell.userNameLabel.text = user["name"] as? String
+        cell.tweetContent.text = tweetArray[indexPath.row]["text"] as? String
+        
+        
+        //extract user's profile image from API
+        let imageUrl = URL(string: (user["profile_image_url_https"] as? String)!)
+        let data = try? Data(contentsOf: imageUrl!)
+        
+        if let imageData = data {
+            cell.profileImageView.image = UIImage(data: imageData)
+        }
+
+        return cell
+    }
+    
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return tweetArray.count
     }
 
  
